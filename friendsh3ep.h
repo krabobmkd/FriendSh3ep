c2e0633e7729f287352940d978ad2fddf3addd00
krabobmkd <victorien.ferry@free.fr>
2026-08-22 13:29:10 +0200

    prepare 1.0 package

--- friendsh3ep.h
+++ friendsh3ep.h
@@ -31,7 +31,7 @@
 #include "fs3esettings.h"
 #include "avatarimages.h"
 
-#define FRIENDSH3EP_VERSION "0.9.0"
+#define FRIENDSH3EP_VERSION "1.0"
 
 /* Login two-phase OAuth state machine */
 typedef enum {
